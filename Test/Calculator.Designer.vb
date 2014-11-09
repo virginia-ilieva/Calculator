b45006f375dad5b2280435a19434eeef024165b8
virginia-ilieva <virginia.ilieva.84@gmail.com>
2014-11-09 20:03:36 +0200

    Calculator - test assignment
    Да се направи опростен вариянт на калкулатура на windows
    само основните функции (+;-;*;/)
    Изпоолзването на контролите от controls.dll е задължително (позволено е
    използването на стандартен EditBox контрол на .NET)
    подредбата на бутоните трябва да е максимално близо до подредбата на
    бутоните на Windows калкулатор.
    
    незадължително но препоръчително би било използването на
    controls.cBtnList
    cBtnList uses data tsble with ID as integer, Label as varchar
    
    Controls.dll не работи с тулбара на MS Visual Studio - може да се
    използва само по начина показан в примера.

---
+++ Test/Calculator.Designer.vb
@@ -0,0 +1,270 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Calculator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New controls.cBtn()
+        Me.Button2 = New controls.cBtn()
+        Me.Button3 = New controls.cBtn()
+        Me.Button4 = New controls.cBtn()
+        Me.Button5 = New controls.cBtn()
+        Me.Button6 = New controls.cBtn()
+        Me.Button7 = New controls.cBtn()
+        Me.Button8 = New controls.cBtn()
+        Me.Button9 = New controls.cBtn()
+        Me.Button10 = New controls.cBtn()
+        Me.Button11 = New controls.cBtn()
+        Me.Button12 = New controls.cBtn()
+        Me.Button13 = New controls.cBtn()
+        Me.Button14 = New controls.cBtn()
+        Me.Button15 = New controls.cBtn()
+        Me.Button16 = New controls.cBtn()
+        Me.Button17 = New controls.cBtn()
+        Me.Button18 = New controls.cBtn()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(13, 141)
+        Me.Button1.Name = "Button1"
+        Me.Button1.TabIndex = 1
+        Me.Button1.Label = "1"
+        Me.Button1.imgPicUp = My.Resources.btn_disabled_8x4_VGA_
+        Me.Button1.imgPicDown = My.Resources.btn_grey_8x4_VGA_
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(57, 141)
+        Me.Button2.Name = "Button2"
+        Me.Button2.TabIndex = 2
+        Me.Button2.Label = "2"
+        Me.Button2.imgPicUp = My.Resources.btn_disabled_8x4_VGA_
+        Me.Button2.imgPicDown = My.Resources.btn_grey_8x4_VGA_
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(102, 141)
+        Me.Button3.Name = "Button3"
+        Me.Button3.TabIndex = 3
+        Me.Button3.Label = "3"
+        Me.Button3.imgPicUp = My.Resources.btn_disabled_8x4_VGA_
+        Me.Button3.imgPicDown = My.Resources.btn_grey_8x4_VGA_
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(13, 100)
+        Me.Button4.Name = "Button4"
+        Me.Button4.TabIndex = 4
+        Me.Button4.Label = "4"
+        Me.Button4.imgPicUp = My.Resources.btn_disabled_8x4_VGA_
+        Me.Button4.imgPicDown = My.Resources.btn_grey_8x4_VGA_
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(57, 100)
+        Me.Button5.Name = "Button5"
+        Me.Button5.TabIndex = 5
+        Me.Button5.Label = "5"
+        Me.Button5.imgPicUp = My.Resources.btn_disabled_8x4_VGA_
+        Me.Button5.imgPicDown = My.Resources.btn_grey_8x4_VGA_
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(102, 100)
+        Me.Button6.Name = "Button6"
+        Me.Button6.TabIndex = 6
+        Me.Button6.Label = "6"
+        Me.Button6.imgPicUp = My.Resources.btn_disabled_8x4_VGA_
+        Me.Button6.imgPicDown = My.Resources.btn_grey_8x4_VGA_
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(12, 59)
+        Me.Button7.Name = "Button7"
+        Me.Button7.TabIndex = 7
+        Me.Button7.Label = "7"
+        Me.Button7.imgPicUp = My.Resources.btn_disabled_8x4_VGA_
+        Me.Button7.imgPicDown = My.Resources.btn_grey_8x4_VGA_
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(57, 59)
+        Me.Button8.Name = "Button8"
+        Me.Button8.TabIndex = 8
+        Me.Button8.Label = "8"
+        Me.Button8.imgPicUp = My.Resources.btn_disabled_8x4_VGA_
+        Me.Button8.imgPicDown = My.Resources.btn_grey_8x4_VGA_
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(102, 59)
+        Me.Button9.Name = "Button9"
+        Me.Button9.TabIndex = 9
+        Me.Button9.Label = "9"
+        Me.Button9.imgPicUp = My.Resources.btn_disabled_8x4_VGA_
+        Me.Button9.imgPicDown = My.Resources.btn_grey_8x4_VGA_
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(12, 182)
+        Me.Button10.Name = "Button10"
+        Me.Button10.TabIndex = 0
+        Me.Button10.Label = "0"
+        Me.Button10.imgPicUp = My.Resources.btn_disabled_long_VGA_
+        Me.Button10.imgPicDown = My.Resources.btn_grey_long_VGA_
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(100, 182)
+        Me.Button11.Name = "Button11"
+        Me.Button11.TabIndex = 10
+        Me.Button11.Label = "."
+        Me.Button11.imgPicUp = My.Resources.btn_disabled_8x4_VGA_
+        Me.Button11.imgPicDown = My.Resources.btn_grey_8x4_VGA_
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(192, 59)
+        Me.Button12.Name = "Button12"
+        Me.Button12.TabIndex = 17
+        Me.Button12.Label = "C"
+        Me.Button12.imgPicUp = My.Resources.btn_disabled_8x4_VGA_
+        Me.Button12.imgPicDown = My.Resources.btn_grey_8x4_VGA_
+        '
+        'Button13
+        '
+        Me.Button13.Location = New System.Drawing.Point(147, 182)
+        Me.Button13.Name = "Button13"
+        Me.Button13.TabIndex = 11
+        Me.Button13.Label = "+"
+        Me.Button13.imgPicUp = My.Resources.btn_disabled_8x4_VGA_
+        Me.Button13.imgPicDown = My.Resources.btn_grey_8x4_VGA_
+        '
+        'Button14
+        '
+        Me.Button14.Location = New System.Drawing.Point(147, 141)
+        Me.Button14.Name = "Button14"
+        Me.Button14.TabIndex = 12
+        Me.Button14.Label = "-"
+        Me.Button14.imgPicUp = My.Resources.btn_disabled_8x4_VGA_
+        Me.Button14.imgPicDown = My.Resources.btn_grey_8x4_VGA_
+        '
+        'Button15
+        '
+        Me.Button15.Location = New System.Drawing.Point(147, 100)
+        Me.Button15.Name = "Button15"
+        Me.Button15.TabIndex = 13
+        Me.Button15.Label = "*"
+        Me.Button15.imgPicUp = My.Resources.btn_disabled_8x4_VGA_
+        Me.Button15.imgPicDown = My.Resources.btn_grey_8x4_VGA_
+        '
+        'Button16
+        '
+        Me.Button16.Location = New System.Drawing.Point(147, 59)
+        Me.Button16.Name = "Button16"
+        Me.Button16.TabIndex = 14
+        Me.Button16.Label = "/"
+        Me.Button16.imgPicUp = My.Resources.btn_disabled_8x4_VGA_
+        Me.Button16.imgPicDown = My.Resources.btn_grey_8x4_VGA_
+        '
+        'Button17
+        '
+        Me.Button17.Location = New System.Drawing.Point(192, 100)
+        Me.Button17.Name = "Button17"
+        Me.Button17.TabIndex = 16
+        Me.Button17.Label = "<--"
+        Me.Button17.imgPicUp = My.Resources.btn_disabled_8x4_VGA_
+        Me.Button17.imgPicDown = My.Resources.btn_grey_8x4_VGA_
+        '
+        'Button18
+        '
+        Me.Button18.Location = New System.Drawing.Point(192, 141)
+        Me.Button18.Name = "Button18"
+        Me.Button18.TabIndex = 15
+        Me.Button18.Label = "="
+        Me.Button18.imgPicUp = My.Resources.btn_disabled_high_VGA_
+        Me.Button18.imgPicDown = My.Resources.btn_grey_high_VGA_
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(219, 29)
+        Me.TextBox1.TabIndex = 18
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Text = "0"
+        '
+        'Calculator
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(242, 225)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button18)
+        Me.Controls.Add(Me.Button17)
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.Button15)
+        Me.Controls.Add(Me.Button14)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Calculator"
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As controls.cBtn
+    Friend WithEvents Button2 As controls.cBtn
+    Friend WithEvents Button3 As controls.cBtn
+    Friend WithEvents Button4 As controls.cBtn
+    Friend WithEvents Button5 As controls.cBtn
+    Friend WithEvents Button6 As controls.cBtn
+    Friend WithEvents Button7 As controls.cBtn
+    Friend WithEvents Button8 As controls.cBtn
+    Friend WithEvents Button9 As controls.cBtn
+    Friend WithEvents Button10 As controls.cBtn
+    Friend WithEvents Button11 As controls.cBtn
+    Friend WithEvents Button12 As controls.cBtn
+    Friend WithEvents Button13 As controls.cBtn
+    Friend WithEvents Button14 As controls.cBtn
+    Friend WithEvents Button15 As controls.cBtn
+    Friend WithEvents Button16 As controls.cBtn
+    Friend WithEvents Button17 As controls.cBtn
+    Friend WithEvents Button18 As controls.cBtn
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+
+End Class
---
+++ Test/Calculator.vb
@@ -0,0 +1,123 @@
+﻿Public Class Calculator
+
+    Dim FirstNumber As Single
+    Dim SecondNumber As Single
+    Dim AnswerNumber As Single
+    Dim ArithmeticProcess As String
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        removeZero()
+        TextBox1.Text = TextBox1.Text & 1
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        removeZero()
+        TextBox1.Text = TextBox1.Text & 2
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        removeZero()
+        TextBox1.Text = TextBox1.Text & 3
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        removeZero()
+        TextBox1.Text = TextBox1.Text & 4
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        removeZero()
+        TextBox1.Text = TextBox1.Text & 5
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        removeZero()
+        TextBox1.Text = TextBox1.Text & 6
+    End Sub
+
+    Private Sub Button7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button7.Click
+        removeZero()
+        TextBox1.Text = TextBox1.Text & 7
+    End Sub
+
+    Private Sub Button8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button8.Click
+        removeZero()
+        TextBox1.Text = TextBox1.Text & 8
+    End Sub
+
+    Private Sub Button9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button9.Click
+        removeZero()
+        TextBox1.Text = TextBox1.Text & 9
+    End Sub
+
+    Private Sub Button10_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button10.Click
+        removeZero()
+        TextBox1.Text = TextBox1.Text & 0
+    End Sub
+
+    Private Sub Button11_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button11.Click
+        TextBox1.Text = TextBox1.Text & "."
+    End Sub
+
+    Private Sub Button12_Click(ByVal Sender As Object, ByVal e As System.EventArgs) Handles Button12.Click
+        TextBox1.Text = "0"
+        FirstNumber = 0
+        SecondNumber = 0
+    End Sub
+
+    Private Sub Button13_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button13.Click
+        FirstNumber = Val(TextBox1.Text)
+        TextBox1.Text = "0"
+        ArithmeticProcess = "+"
+    End Sub
+
+    Private Sub Button14_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button14.Click
+
+
+        FirstNumber = Val(TextBox1.Text)
+        TextBox1.Text = "0"
+        ArithmeticProcess = "-"
+    End Sub
+
+    Private Sub Button15_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button15.Click
+        FirstNumber = Val(TextBox1.Text)
+        TextBox1.Text = "0"
+        ArithmeticProcess = "X"
+    End Sub
+
+    Private Sub Button16_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button16.Click
+        FirstNumber = Val(TextBox1.Text)
+        TextBox1.Text = "0"
+        ArithmeticProcess = "/"
+    End Sub
+
+    Private Sub Button17_Click(ByVal Sender As Object, ByVal e As System.EventArgs) Handles Button17.Click
+        If TextBox1.Text.Length = 1 Then
+            TextBox1.Text = "0"
+        Else
+            TextBox1.Text = TextBox1.Text.Remove(TextBox1.Text.Length - 1, 1)
+        End If
+    End Sub
+
+    Private Sub Button18_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button18.Click
+        SecondNumber = Val(TextBox1.Text)
+        Select Case ArithmeticProcess
+            Case "+"
+                AnswerNumber = FirstNumber + SecondNumber
+            Case "-"
+                AnswerNumber = FirstNumber - SecondNumber
+            Case "X"
+                AnswerNumber = FirstNumber * SecondNumber
+            Case "/"
+                AnswerNumber = FirstNumber / SecondNumber
+        End Select
+        TextBox1.Text = AnswerNumber
+    End Sub
+
+    Private Sub removeZero()
+        If TextBox1.Text = "0" Then
+            TextBox1.Text = ""
+        End If
+    End Sub
+
+End Class
